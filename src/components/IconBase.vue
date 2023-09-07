31faf09c4aad11168c6a7afa35fb7cd35487dbb4
Yan Logan <yanlogans@gmail.com>
2023-09-07 16:22:53 +0300

    feat: add icon components

---
+++ src/components/IconBase.vue
@@ -0,0 +1,50 @@
+<script>
+import IconSearch from "./icons/IconSearch.vue";
+import IconRemove from "./icons/IconRemove.vue";
+
+const icons = {
+  search: IconSearch,
+  remove: IconRemove,
+};
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+      validator(value) {
+        return Object.prototype.hasOwnProperty.call(icons, value);
+      },
+    },
+  },
+
+  computed: {
+    iconComponent() {
+      return icons[this.name];
+    },
+  },
+};
+</script>
+
+<template>
+  <i class="icon" :class="`icon-${name}`">
+    <component :is="iconComponent" role="presentation" />
+  </i>
+</template>
+
+<style lang="scss">
+.icon {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  svg * {
+    transition: all 0.3s;
+  }
+}
+
+.icon-remove:hover {
+  svg > g {
+    opacity: 1;
+  }
+}
+</style>
