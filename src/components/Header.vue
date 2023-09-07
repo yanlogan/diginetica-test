58bdb1a5e9caeaed9f285ad26af18f7b0c155261
Yan Logan <yanlogans@gmail.com>
2023-09-07 16:23:32 +0300

    feat: add header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,54 @@
-<script setup></script>
+<script setup>
+import Logo from "./Logo.vue";
+import Search from "./Search.vue";
+</script>
 
 <template>
-  <header>I am the Header</header>
+  <header class="header">
+    <Logo />
+    <a class="header__catalog" href="#">Каталог</a>
+    <Search />
+    <nav class="header__nav">
+      <ul>
+        <li><a href="#">Информация о компании</a></li>
+        <li><a href="#">Контакты</a></li>
+        <li><a href="#">Полезные ссылки</a></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.header {
+  display: flex;
+  align-items: stretch;
+  padding: 12px 0;
+  font-family: "Inter", sans-serif;
+  font-size: 14px;
+}
+.header__catalog {
+  display: flex;
+  align-items: center;
+  margin-left: 8px;
+  padding: 8px 24px;
+  font-weight: 500;
+  border: 1px solid $color-border;
+  border-radius: 8px;
+
+  &:hover {
+    color: $color-font-main;
+    border-color: $color-border-hover;
+  }
+}
+.header__nav {
+  display: flex;
+  align-items: center;
+
+  ul {
+    @include list(r, 20px);
+    a:not(:hover) {
+      color: #393939;
+    }
+  }
+}
+</style>
