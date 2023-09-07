ba0bb6e711633747d3b80b08140d4392f408f2aa
Yan Logan <yanlogans@gmail.com>
2023-09-07 16:23:19 +0300

    feat: add header search component

---
+++ src/components/Search.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { ref, computed } from "vue";
+import IconBase from "./IconBase.vue";
+
+const inputField = ref(null);
+const inputValue = ref("");
+const isInputNotEmpty = computed(() => inputValue.value.length > 0);
+
+const resetForm = () => {
+  inputValue.value = "";
+  inputField.value.focus();
+};
+</script>
+
+<template>
+  <form class="search" @reset="resetForm">
+    <IconBase name="search" />
+    <div class="search__input-wrapper">
+      <input
+        ref="inputField"
+        type="search"
+        name="header-search"
+        id="header-search"
+        class="search__input"
+        placeholder="Поиск по 100 000 товаров"
+        v-model="inputValue"
+      />
+      <button
+        type="reset"
+        class="search__resetBtn btn"
+        v-show="isInputNotEmpty"
+      >
+        <IconBase name="remove" />
+      </button>
+    </div>
+    <button
+      type="submit"
+      class="search__actionBtn btn"
+      v-show="isInputNotEmpty"
+    >
+      Найти
+    </button>
+  </form>
+</template>
+
+<style lang="scss">
+.search {
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  margin: 0 24px;
+  padding: 4px 4px 4px 16px;
+  border: 1px solid $color-border;
+  border-radius: 10px;
+  transition: all 0.3s;
+
+  &:hover,
+  &:focus-within {
+    border-color: $color-border-hover;
+  }
+}
+.search__input-wrapper {
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  margin: 0 8px;
+}
+.search__input {
+  width: 100%;
+  line-height: 150%;
+
+  &::placeholder {
+    color: $color-font-second;
+  }
+}
+.search__resetBtn {
+  margin-left: 8px;
+}
+.search__actionBtn {
+  padding: 12px 16px;
+  font-family: "PT Sans", sans-serif;
+  font-weight: 700;
+  line-height: 100%;
+  color: $color-white;
+  background-color: $color-border-hover;
+  border-radius: 8px;
+}
+</style>
