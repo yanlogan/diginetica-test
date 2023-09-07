bc3a32bcb9767c9e7f90b51437415f0895a06b90
Yan Logan <yanlogans@gmail.com>
2023-09-07 17:36:17 +0300

    create main app grid

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,27 @@ import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <Header />
+  <header class="header-wrapper">
+    <Header />
+  </header>
+  <aside class="filters-wrapper">
+    <!-- filters -->
+    filters
+  </aside>
+  <main class="search-results-wrapper">
+    <!-- search results -->
+    search results
+  </main>
 </template>
+
+<style scoped>
+.header-wrapper {
+  grid-area: header;
+}
+.filters-wrapper {
+  grid-area: filters;
+}
+.search-results-wrapper {
+  grid-area: results;
+}
+</style>
