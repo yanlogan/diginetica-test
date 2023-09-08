2983bd665bea3c147852d8d123895393ad8d4f66
Yan Logan <yanlogans@gmail.com>
2023-09-08 16:51:03 +0300

    feat: add filter section

---
+++ src/components/FilterSection.vue
@@ -0,0 +1,66 @@
+<script setup>
+const props = defineProps({
+  classes: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  title: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  needsClearSelection: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <section class="filter-section" :class="classes">
+    <header
+      class="filter-section__header"
+      :class="{ 'filter-section__header--clear': needsClearSelection }"
+      v-show="title"
+    >
+      <span class="filter-section__title" v-show="title">{{ title }}</span>
+      <button
+        class="filter-section__clearSelectionBtn"
+        v-show="needsClearSelection"
+      >
+        Очистить
+      </button>
+    </header>
+    <slot></slot>
+  </section>
+</template>
+
+<style lang="scss">
+.filter-section {
+  margin-bottom: 28px;
+}
+
+.filter-section__header {
+  display: flex;
+  justify-content: center;
+
+  &--clear {
+    justify-content: space-between;
+  }
+}
+
+.filter-section__title {
+  font-size: $font-size-big;
+  line-height: 125%;
+  font-weight: 700;
+}
+
+.filter-section__clearSelectionBtn {
+  color: $color-font-second;
+  font-size: $font-size-small;
+  line-height: 100%;
+  text-decoration: underline;
+}
+</style>
