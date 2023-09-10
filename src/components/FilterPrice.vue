125e1f08c49f6c528de8f4f5fff92f3b8d4b2bbb
Yan Logan <yanlogans@gmail.com>
2023-09-10 22:08:15 +0300

    feat: add price filter section

---
+++ src/components/FilterPrice.vue
@@ -0,0 +1,29 @@
+<script setup>
+import FilterSection from "./FilterSection.vue";
+import InputPrice from "./InputPrice.vue";
+</script>
+
+<template>
+  <FilterSection classes="filter-price" title="Цена">
+    <div class="filter-price-wrapper">
+      <InputPrice classes="filter-price__input" id="price-from" label="от" />
+      <div class="filter-price__divider"></div>
+      <InputPrice classes="filter-price__input" id="price-to" label="до" />
+    </div>
+  </FilterSection>
+</template>
+
+<style lang="scss">
+.filter-price-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.filter-price__divider {
+  width: 10px;
+  height: 1px;
+  margin: 0 16px;
+  flex-shrink: 0;
+  background-color: $color-border;
+}
+</style>
